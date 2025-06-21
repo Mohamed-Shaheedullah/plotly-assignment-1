ae092c742be7ef8575ac43c1b841fea84c9637e3
Mohamed Shaheedullah <conifer567@outlook.com>
2025-06-21 08:21:47 +0100

    added radio buttons

--- app1/app.py
+++ app1/app.py
@@ -4,4 +4,28 @@ import plotly.express as px
 import dash_ag_grid as dag
 
 df = pd.read_csv(r"C:\Users\conif\Documents\Projects\Python\python-data\shades.csv")
-print(df.head())
+my_group = df['group'].unique().tolist()
+my_group.sort()
+
+
+# Create a Dash application
+# and add a dropdown component to select a brand
+app = Dash()
+app.layout = html.Div([
+    dcc.Dropdown(df.brand.unique(),'Revlon', id='demo-dropdown'),
+    html.Div(id='dd-output-container'),
+    dcc.RadioItems(options=my_group, id='radio-items')
+])
+
+@callback(
+    Output('dd-output-container', 'children'),
+    Input('demo-dropdown', 'value'),
+    Input('radio-items', 'value')
+)
+def update_output(dropdown_value, radio_value):
+    return f'You have selected brand: {dropdown_value} and group: {radio_value}'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
